1effce7ad7ed343d0315b3039cafe4cacd8ed6b5
goodwin <b1ll.cipher@yandex.ru>
2017-05-20 02:25:59 +0300

    Update fix send message about position player

--- YorozuyaGS/ViewInvisible.cpp
+++ YorozuyaGS/ViewInvisible.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
 
+#include "ETypes.h"
 #include "ViewInvisible.h"
 #include <ATF/global.hpp>
-
+#include <ATF/_player_fixpositon_zocl.hpp>
 
 namespace GameServer
 {
@@ -44,24 +45,45 @@ namespace GameServer
         }
 
         // SendMsg_StateInform
-        // SendMsg_FixPosition
         // SendMsg_OtherShapePart
         // SendMsg_OtherShapeAll
         void WINAPIV CViewInvisible::Player_SendMsg_FixPosition(
             ATF::CPlayer* pPlayer,
-            int n,
+            int dwClientIndex,
             ATF::info::CPlayerSendMsg_FixPosition752_ptr next)
         {
-            CPlayer* pDstPlayer = &global::g_Player[n];
-            if (pPlayer->m_EP.GetEff_State((int)ATF::_EFF_STATE::Invisible))
-            {
-                if (!pDstPlayer->m_EP.GetEff_Plus((int)ATF::_EFF_PLUS::Detect))
-                {
-                    return;
-                }
-            }
+            UNREFERENCED_PARAMETER(next);
 
-            next(pPlayer, n);
+            CPlayer* pDstPlayer = &global::g_Player[dwClientIndex];
+
+            bool bPassed = false;
+            do
+            {
+                if (pPlayer->m_bObserver && !pDstPlayer->m_byUserDgr)
+                    break;
+
+                if (pPlayer->m_EP.GetEff_State((int)ATF::_EFF_STATE::Invisible) &&
+                    !pDstPlayer->m_EP.GetEff_Plus((int)ATF::_EFF_PLUS::Detect))
+                    break;
+
+                bPassed = true;
+            } while (false);
+
+            if (bPassed)
+            {
+                char pbyType[2]{ 4, 9 };
+                ATF::_player_fixpositon_zocl szMsg;
+
+                szMsg.wIndex = pPlayer->m_ObjID.m_wIndex;
+                szMsg.dwSerial = pPlayer->m_dwObjSerial;
+                szMsg.wEquipVer = pPlayer->GetVisualVer();
+                ATF::global::FloatToShort(pPlayer->m_fCurPos, szMsg.zCur, 3);
+                szMsg.byRaceCode = pPlayer->m_Param.GetRaceSexCode();
+                szMsg.dwStateFlag = pPlayer->GetStateFlag();
+                szMsg.wLastEffectCode = pPlayer->m_wLastContEffect;
+                szMsg.byColor = pPlayer->m_byGuildBattleColorInx;
+                ATF::global::g_NetProcess[(uint8_t)e_type_line::client]->LoadSendMsg(dwClientIndex, pbyType, (char *)&szMsg, sizeof(szMsg));
+            }
         }
     }
 }
