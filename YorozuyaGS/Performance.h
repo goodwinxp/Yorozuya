ae8d4fb51a403238487b2d7264b55dd6d58c3bf3
goodwinxp <b1ll.cipher@yandex.ru>
2017-11-05 12:30:49 +0300

    added optimization

--- YorozuyaGS/Performance.h
+++ YorozuyaGS/Performance.h
@@ -27,6 +27,21 @@ namespace GameServer
                 float* fPos,
                 float* fTar,
                 ATF::Global::Info::GetSqrt667_ptr next);
+
+            static float WINAPIV Get3DSqrt(
+                float* fPos,
+                float* fTar,
+                ATF::Global::Info::Get3DSqrt435_ptr next);
+
+            static float WINAPIV GetYAngle(
+                float* fPos,
+                float* fTar,
+                ATF::Global::Info::GetYAngle700_ptr next);
+
+            static float WINAPIV GetDist(
+                const float *fPos,
+                const float *fTar,
+                ATF::Global::Info::GetDist498_ptr next);
         };
     };
 };
