17b10397a1a27f88d718e5eaabbc9b3a4d4fc4ca
goodwinxp <b1ll.cipher@yandex.ru>
2017-12-19 19:12:28 +0300

    bump version

--- YorozuyaGS/Common/version.h
+++ YorozuyaGS/Common/version.h
@@ -6,7 +6,7 @@
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               0
 #define VERSION_REVISION            0
-#define VERSION_BUILD               0
+#define VERSION_BUILD               1
 
 #define VER_FILE_DESCRIPTION_STR    "Defense for server rf online 2232"
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
