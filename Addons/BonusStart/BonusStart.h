23ba1ccc3e22886b1b922f26d27d064ba1f21072
goodwinxp <b1ll.cipher@yandex.ru>
2017-12-26 15:41:26 +0300

    Mini refactoring in bonus start

--- Addons/BonusStart/BonusStart.h
+++ Addons/BonusStart/BonusStart.h
@@ -8,27 +8,30 @@ namespace GameServer
 {
     namespace Addon
     {
-		class CBonusStart
-			: public Yorozuya::Module::IModule
-			, CModuleHook
-		{
-		public:
-			CBonusStart() { };
+        class CBonusStart
+            : public Yorozuya::Module::IModule
+            , CModuleHook
+        {
+        public:
+            CBonusStart() { };
 
-			virtual void load() override;
+            virtual void load() override;
 
-			virtual void unload() override;
+            virtual void unload() override;
 
-			virtual Yorozuya::Module::ModuleName_t get_name() override;
+            virtual Yorozuya::Module::ModuleName_t get_name() override;
 
-			virtual void configure(const rapidjson::Value& nodeConfig) override;
-		private:
-			static bool m_bActivated;
-			static int32_t m_nStartLvl;
-			static uint32_t m_nStartDalant;
-			static uint32_t m_nStartGold;
+            virtual void configure(const rapidjson::Value& nodeConfig) override;
+        private:
+            static bool m_bActivated;
+            static int32_t m_nStartLvl;
+            static uint32_t m_nStartDalant;
+            static uint32_t m_nStartGold;
 
-			static void WINAPIV CreateComplete(ATF::CPlayer *_this, ATF::Info::CPlayerCreateComplete102_ptr next);
-		};
+        private:
+            static void WINAPIV CreateComplete(
+                ATF::CPlayer *_this,
+                ATF::Info::CPlayerCreateComplete102_ptr next);
+        };
     };
 };
