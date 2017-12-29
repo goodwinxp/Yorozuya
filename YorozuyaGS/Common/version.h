3d75a604a7eb4619e19cc82bb16c8fcb11f352ad
goodwinxp <b1ll.cipher@yandex.ru>
2017-12-30 01:16:45 +0300

    bump version

--- YorozuyaGS/Common/version.h
+++ YorozuyaGS/Common/version.h
@@ -4,9 +4,9 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 
 #define VERSION_MAJOR               1
-#define VERSION_MINOR               0
+#define VERSION_MINOR               1
 #define VERSION_REVISION            0
-#define VERSION_BUILD               1
+#define VERSION_BUILD               0
 
 #define VER_FILE_DESCRIPTION_STR    "Defense for server rf online 2232"
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
