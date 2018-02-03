a149813d454028ce5c9473e404b9d5194b48b6fd
goodwinxp <b1ll.cipher@yandex.ru>
2018-02-04 02:55:34 +0300

    Updated performance fix

--- YorozuyaGS/MainThread/MainThread.cpp
+++ YorozuyaGS/MainThread/MainThread.cpp
@@ -7,6 +7,281 @@ namespace GameServer
 {
     namespace Fixes
     {
+        CMainThread::CMainThread()
+        {
+            using namespace detail;
+
+            m_Tasks.push_back(CTask{ [] { ATF::Global::g_MapOper->OnLoop(); } });
+
+            m_Tasks.push_back(CTask{ [] { ATF::Global::g_Network->OnLoop(); } });
+
+            m_Tasks.push_back(CTask{ [] { ATF::Global::g_MainThread->DQSCompleteProcess(); } });
+
+            m_Tasks.push_back(CTask{ [] { ATF::CCashDBWorkManager::Instance()->CompleteWork(); } });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(500) },
+                [] { ATF::Global::g_HolySys->OnLoop(); } 
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(5000) },
+                [] { ATF::Global::g_MainThread->CheckAccountLineState(); } 
+                });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::Global::g_MainThread->CheckAvatorState(); } 
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::Global::g_MainThread->ContUserSaveJobCheck(); } 
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::Global::g_MainThread->CheckDayChangedPvpPointClear(); } 
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::Global::g_MainThread->CheckRadarItemDelay(); } 
+            });
+            
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::Global::g_MainThread->m_GuildCreateEventInfo.Loop(); } 
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::Global::g_MainThread->m_pRFEvent_ClassRefine->vfptr->Loop(ATF::Global::g_MainThread->m_pRFEvent_ClassRefine); } 
+            });
+            
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { ATF::Global::g_DarkHoleQuest->CheckQuestOnLoop(); } 
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::Global::g_WorldSch->Loop(); } 
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                []
+                {
+                    for (int j = 0; j < 3; ++j)
+                        ATF::Global::g_TransportShip[j].Loop();
+                } 
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                []
+                {
+                    bool pbChangeDay = false;
+                    ATF::Global::eUpdateEconomySystem(&pbChangeDay);
+                    if (pbChangeDay)
+                        ATF::Global::g_GameStatistics->ConvertDay(ATF::Global::g_MainThread->m_szWorldName);
+                    ATF::Global::OnLoop_GuildSystem(pbChangeDay);
+                }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::Global::OnLoop_VoteSystem(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CPvpUserAndGuildRankingSystem::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CRaceBossMsgController::Instance()->OnLoop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CReturnGateController::Instance()->OnLoop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CRecallEffectController::Instance()->OnLoop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(500) },
+                [] { ATF::CGuildBattleController::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::AutoMineMachineMng::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(500) },
+                [] { ATF::CUnmannedTraderController::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CLogTypeDBTaskManager::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CGuildRoomSystem::GetInstance()->RentRoomTimer(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CGuildRoomSystem::GetInstance()->RentRoomTimer(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { ATF::CPostSystemManager::Instace()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CItemStoreManager::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::PatriarchElectProcessor::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CMoveMapLimitManager::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CRaceBuffManager::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { ATF::CNuclearBombMgr::Instance()->vfptr->Loop(ATF::CNuclearBombMgr::Instance()); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CHonorGuild::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::COreAmountMgr::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CExchangeEvent::Instance()->vfptr->Loop(ATF::CExchangeEvent::Instance()); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CNationSettingManager::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { ATF::CLuaScriptMgr::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { ATF::CashItemRemoteStore::Instance()->Loop_TatalCashEvent(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { ATF::TimeLimitMgr::Instance()->Chack_Time(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CActionPointSystemMgr::Instance()->Check_Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CGoldenBoxItemMgr::Instance()->Loop_Event(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CMoneySupplyMgr::Instance()->LoopMoneySupply(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CTotalGuildRankManager::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CWeeklyGuildRankManager::Instance()->Loop(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(2000) },
+                [] { ATF::CAsyncLogger::Instance()->Loop(); }
+            });
+            
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { ATF::Global::g_MainThread->CheckConnNumLog(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { ATF::Global::g_MainThread->m_GameMsg.PumpMsgList(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { ATF::Global::g_MainThread->CheckServerRateINIFile(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { ATF::Global::g_MainThread->ForceCloseUserInTiming(); }
+            });
+
+            m_Tasks.push_back(CTask{
+                CRunRule{ ::std::chrono::milliseconds(1000) },
+                [] { 
+                    if (ATF::Global::GetLoopTime() - *ATF::Global::dwTime_AliveMonNum > 60000)
+                    {
+                        *ATF::Global::dwTime_AliveMonNum = ATF::Global::GetLoopTime();
+                        int nDiedMonster = 0;
+                        for (int k = 0; k < 30000; ++k)
+                        {
+                            if (!ATF::Global::g_Monster[k].m_bLive)
+                                ++nDiedMonster;
+                        }
+
+                        if (*ATF::Global::dwMaxMonNum < nDiedMonster)
+                        {
+                            *ATF::Global::dwMaxMonNum = nDiedMonster;
+                        }
+                    }
+                }
+            });
+        }
+
         void CMainThread::load()
         {
             enable_hook(&ATF::CMainThread::CheckForceClose, &CMainThread::CheckForceClose);
@@ -15,9 +290,6 @@ namespace GameServer
 
         void CMainThread::unload()
         {
-            if (m_thRuleThread.joinable())
-                m_thRuleThread.join();
-
             cleanup_all_hook();
         }
 
@@ -27,13 +299,6 @@ namespace GameServer
             return name;
         }
 
-        void CMainThread::zone_start()
-        {
-            m_thRuleThread = std::move(std::thread([&]() {
-                CMainThread::MyRuleThread();
-            }));
-        }
-
         void WINAPIV CMainThread::CheckForceClose(
             ATF::CMainThread * pObj,
             ATF::Info::CMainThreadCheckForceClose24_ptr next)
@@ -41,112 +306,16 @@ namespace GameServer
             UNREFERENCED_PARAMETER(pObj);
             UNREFERENCED_PARAMETER(next);
         }
-
+        
         void WINAPIV CMainThread::OnRun(
             ATF::CMainThread* pObj,
             ATF::Info::CMainThreadOnRun130_ptr next)
         {
-            ATF::Global::g_Network->OnLoop();
-            ATF::Global::g_MainThread->DQSCompleteProcess();
-            ATF::CCashDBWorkManager::Instance()->CompleteWork();
+            UNREFERENCED_PARAMETER(next);
+            auto spModule = GetModule<CMainThread>();
 
-            ATF::Global::g_MainThread->CheckAccountLineState();
-        }
-
-        void WINAPIV CMainThread::MyRuleThread()
-        {
-            ::srand(::time(nullptr));
-            int nTickCount = 0;
-            while (ATF::Global::g_MainThread->m_bRuleThread)
-            {
-                *ATF::Global::g_dwCurTime = timeGetTime();
-                ATF::Global::g_MainThread->m_MainFrameRate.CalcSpeedPerFrame();
-                CMainThread::MyOnRun();
-                if (!ATF::Global::g_MainThread->m_nSleepIgnore &&
-                    ++nTickCount > ATF::Global::g_MainThread->m_nSleepTerm)
-                {
-                    Sleep(ATF::Global::g_MainThread->m_nSleepValue);
-                    nTickCount = 0;
-                }
-            }
-        }
-
-        void WINAPIV CMainThread::MyOnRun()
-        {
-            //ATF::CMapDisplay::DrawDisplay(&ATF::Global::g_MapDisplay);
-            ATF::Global::g_MapOper->OnLoop();
-            ATF::Global::g_HolySys->OnLoop();
-
-            ATF::Global::g_MainThread->CheckAvatorState();
-            ATF::Global::g_MainThread->ForceCloseUserInTiming();
-            bool pbChangeDay = false;
-            ATF::Global::eUpdateEconomySystem(&pbChangeDay);
-            if ( pbChangeDay )
-                ATF::Global::g_GameStatistics->ConvertDay(ATF::Global::g_MainThread->m_szWorldName);
-
-            ATF::CMoneySupplyMgr::Instance()->LoopMoneySupply();
-            ATF::Global::g_MainThread->CheckConnNumLog();
-            ATF::Global::g_MainThread->m_GameMsg.PumpMsgList();
-            ATF::CPvpUserAndGuildRankingSystem::Instance()->Loop();
-            ATF::Global::g_MainThread->ContUserSaveJobCheck();
-            ATF::Global::OnLoop_VoteSystem();
-            ATF::Global::OnLoop_GuildSystem(pbChangeDay);
-            ATF::Global::g_DarkHoleQuest->CheckQuestOnLoop();
-            ATF::CRaceBossMsgController::Instance()->OnLoop();
-            ATF::CReturnGateController::Instance()->OnLoop();
-            ATF::CRecallEffectController::Instance()->OnLoop();
-            ATF::Global::g_WorldSch->Loop();
-            for (int j = 0; j < 3; ++j)
-                ATF::Global::g_TransportShip[j].Loop();
-
-            ATF::CGuildBattleController::Instance()->Loop();
-            ATF::CTotalGuildRankManager::Instance()->Loop();
-            ATF::CWeeklyGuildRankManager::Instance()->Loop();
-            ATF::AutoMineMachineMng::Instance()->Loop();
-            
-            if (ATF::Global::GetLoopTime() - *ATF::Global::dwTime_AliveMonNum > 60000 )
-            {
-                *ATF::Global::dwTime_AliveMonNum = ATF::Global::GetLoopTime();
-                int nDiedMonster = 0;
-                for (int k = 0; k < 30000; ++k )
-                {
-                    if ( !ATF::Global::g_Monster[k].m_bLive )
-                        ++nDiedMonster;
-                }
-
-                if (*ATF::Global::dwMaxMonNum < nDiedMonster)
-                {
-                    *ATF::Global::dwMaxMonNum = nDiedMonster;
-                }
-            }
-
-            ATF::CUnmannedTraderController::Instance()->Loop();
-            ATF::CLogTypeDBTaskManager::Instance()->Loop();
-            ATF::Global::g_MainThread->CheckDayChangedPvpPointClear();
-            ATF::CGuildRoomSystem::GetInstance()->RentRoomTimer();
-            ATF::Global::g_MainThread->CheckRadarItemDelay();
-            ATF::CPostSystemManager::Instace()->Loop();
-            ATF::CItemStoreManager::Instance()->Loop();
-            ATF::PatriarchElectProcessor::Instance()->Loop();
-            ATF::CMoveMapLimitManager::Instance()->Loop();
-            ATF::CRaceBuffManager::Instance()->Loop();
-            ATF::CAsyncLogger::Instance()->Loop();
-
-            ATF::CNuclearBombMgr::Instance()->vfptr->Loop(ATF::CNuclearBombMgr::Instance());
-            ATF::CHonorGuild::Instance()->Loop();
-            ATF::COreAmountMgr::Instance()->Loop();
-            ATF::CExchangeEvent::Instance()->vfptr->Loop(ATF::CExchangeEvent::Instance());
-            ATF::Global::g_MainThread->m_GuildCreateEventInfo.Loop();
-            ATF::Global::g_MainThread->m_pRFEvent_ClassRefine->vfptr->Loop(
-                ATF::Global::g_MainThread->m_pRFEvent_ClassRefine);
-
-            ATF::CNationSettingManager::Instance()->Loop();
-            ATF::CLuaScriptMgr::Instance()->Loop();
-            ATF::Global::g_MainThread->CheckServerRateINIFile();
-            ATF::CashItemRemoteStore::Instance()->Loop_TatalCashEvent();
-            ATF::TimeLimitMgr::Instance()->Chack_Time();
-            ATF::CActionPointSystemMgr::Instance()->Check_Loop();
-            ATF::CGoldenBoxItemMgr::Instance()->Loop_Event();
+            for (const auto& t : spModule->m_Tasks)
+                t.run();
         }
     }
 }
--- YorozuyaGS/MainThread/MainThread.h
+++ YorozuyaGS/MainThread/MainThread.h
@@ -2,6 +2,7 @@
 
 #include "../Common/ModuleRegistry.h"
 #include "../../Common/Interfaces/ModuleInterface.h"
+#include "../../Common/Helpers/TimeHelper.hpp"
 
 #include <ATF/CMainThreadInfo.hpp>
 #include <ATF/CExchangeEvent.hpp>
@@ -10,12 +11,85 @@ namespace GameServer
 {
     namespace Fixes
     {
+        namespace detail
+        {
+            class CRunRule
+            {
+            public:
+                CRunRule()
+                    : CRunRule(false)
+                {
+                }
+
+                CRunRule(::std::chrono::milliseconds interval)
+                    : CRunRule(true, interval)
+                {
+                }
+
+                bool runnable() const
+                {
+                    bool result = true;
+                    if (m_bEnabledTimer)
+                    {
+                        result = m_timer.is_end();
+                        if (result)
+                            m_timer.begin(m_interval);
+                    }
+
+                    return result;
+                }
+
+            private:
+                CRunRule(bool enabled_timer, ::std::chrono::milliseconds interval = ::std::chrono::milliseconds(0))
+                    : m_bEnabledTimer(enabled_timer)
+                    , m_interval(interval)
+                {
+                    if (m_bEnabledTimer)
+                    {
+                        m_timer.begin(m_interval);
+                    }
+                }
+
+            private:
+                const bool m_bEnabledTimer;
+                const ::std::chrono::milliseconds m_interval;
+                mutable TimeHelper::CTimer m_timer;
+            };
+
+            class CTask
+            {
+            public:
+                CTask(const CRunRule& rule, const ::std::function<void()>& impl)
+                    : m_ruleRun(rule)
+                    , m_fnImpl(impl)
+                {
+                }
+
+                CTask(const ::std::function<void()>& impl)
+                    : CTask(CRunRule{}, impl)
+                {
+                }
+
+                void run() const
+                {
+                    if (m_ruleRun.runnable())
+                        m_fnImpl();
+                }
+
+            private:
+                const CRunRule m_ruleRun;
+                const ::std::function<void()> m_fnImpl;
+            };
+
+            using Tasks_t = ::std::vector<CTask>;
+        }
+
         class CMainThread
             : public Yorozuya::Module::IModule
             , CModuleRegister<CMainThread>
         {
         public:
-            CMainThread() { };
+            CMainThread();
 
             virtual void load() override;
 
@@ -23,10 +97,8 @@ namespace GameServer
 
             virtual Yorozuya::Module::ModuleName_t get_name() override;
 
-            virtual void zone_start() override;
-
         private:
-            ::std::thread m_thRuleThread;
+            detail::Tasks_t m_Tasks;
 
         private:
             static void WINAPIV CheckForceClose(
@@ -36,10 +108,6 @@ namespace GameServer
             static void WINAPIV OnRun(
                 ATF::CMainThread* pObj,
                 ATF::Info::CMainThreadOnRun130_ptr next);
-
-            void WINAPIV MyRuleThread();
-
-            void WINAPIV MyOnRun();
         };
     };
 };
